91da1424e366338eb09d421c00bc8ad5b83dec96
Samir Anand <samiranand@Samirs-MacBook-Pro.local>
2023-01-19 15:19:29 +0530

    adding action for automation

--- generator.sh
+++ generator.sh
@@ -1,3 +1,13 @@
+gem install bundler
 cd mmv1
+bundle install
+./tools/doctor
+
+make terraform VERSION=ga OUTPUT_PATH="$GOPATH/src/github.com/hashicorp/terraform-provider-google"
+make terraform VERSION=beta OUTPUT_PATH="$GOPATH/src/github.com/hashicorp/terraform-provider-google-beta"
+
+# Only generate a specific product (plus all common files)
+make terraform VERSION=ga OUTPUT_PATH="$GOPATH/src/github.com/hashicorp/terraform-provider-google" PRODUCT=dataproc
+
 rm -rf build/inspec
 ruby compiler.rb -e inspec -p products/kms -t Location -o build/inspec
