c16171c1bbadba7122d23e6e573ab9c1564e98a3
Samir Anand <sam.sunny001@gmail.com>
2023-08-29 08:09:42 +0530

    Automatically generated by magic modules for service: compute and resource: RegionSecurityPolicy. This commit includes the following changes: - Singular Resource ERB File - Plural Resource ERB File - Terraform configuration - api.yaml configuration for product compute and resource RegionSecurityPolicy
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
+++ mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
@@ -1483,3 +1483,12 @@ resource "google_compute_image" "example" {
   }
 }
 
+
+resource "google_compute_image" "example" {
+  name = "example-image-${local.name_suffix}"
+
+  raw_disk {
+    source = "https://storage.googleapis.com/bosh-gce-raw-stemcells/bosh-stemcell-97.98-google-kvm-ubuntu-xenial-go_agent-raw-1557960142.tar.gz"
+  }
+}
+
