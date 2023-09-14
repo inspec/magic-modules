13b0dee10bdcc47d2a3250446c8e8cbd0894abdf
Samir Anand <sam.sunny001@gmail.com>
2023-09-14 12:42:55 +0530

    Automatically generated by magic modules for service: vertex_ai and resource: Index. This commit includes the following changes: - Singular Resource ERB File - Plural Resource ERB File - Terraform configuration - api.yaml configuration for product vertex_ai and resource Index
    Signed-off-by: Samir Anand <sam.sunny001@gmail.com>

--- mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
+++ mmv1/templates/inspec/tests/integration/build/gcp-mm.tf
@@ -1359,3 +1359,16 @@ resource "google_healthcare_dataset" "default" {
   time_zone = "UTC"
 }
 
+
+resource "google_datastore_index" "default" {
+  kind = "foo"
+  properties {
+    name = "property_a-${local.name_suffix}"
+    direction = "ASCENDING"
+  }
+  properties {
+    name = "property_b-${local.name_suffix}"
+    direction = "ASCENDING"
+  }
+}
+
